12728d31e7c1ace6d74547fe09ee27ea8fec41f2
Saul Page <saulable@gmail.com>
2018-08-30 16:56:39 +0100

    weird commit

---
+++ client/src/components/Tracks/Dashboard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import LeftNavigation from '../../containers/LeftNavigation';
+import ProjectAutoSuggest from '../Calendar/ProjectAutoSuggest';
+
+class TracksDashboard extends Component {
+	render() {
+		return (
+			<div>
+				<LeftNavigation />
+				<div className="container-fluid">
+					<div className="card">
+						<ProjectAutoSuggest />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TracksDashboard);
