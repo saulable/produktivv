f7938b6cdb661e3479553bb4288acb502355d51c
Saul Page <saulable@gmail.com>
2018-09-29 14:23:54 +0100

    updated eslint.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-    "extends": "airbnb"
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true
+	},
+	extends: 'eslint:recommended',
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module',
+		allowImportExportEverywhere: false,
+		codeFrame: false
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'no-unused-vars': 0,
+		'no-console': 0,
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		globals: {
+			__: false
+		}
+	}
 };
